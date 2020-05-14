57d5bd8ca8e3a8649abf07050b5f43dc3208f277
Kethantummala <tummala.kethan@gmail.com>
2020-05-14 18:01:50 +0530

    Completed LL STL

--- C++/List.cpp
+++ C++/List.cpp
@@ -49,13 +49,56 @@ void reverse_Blocks(auto List1,auto& List2,int k) {
 	}
 }
 
-void floyds_cycle_remove_optimized(auto L1) {
-	
+list<int> sum_Dir1(auto L1,auto L2) {
+	list<int> ans;
+	int carry=0;
+	auto it1=L1.begin(),it2=L2.begin();
+	while(it1!=L1.end() || it2!=L2.end()) {
+		int a,b;
+		if(it1==L1.end()) {
+			a=0;
+			b=*it2;
+			it2++;
+		}
+		else if(it2==L2.end()) {
+			b=0;
+			a=*it1;
+			it1++;
+		}
+		else {
+			a=*it1;
+			b=*it2;
+			it1++;
+			it2++;
+		}
+		a=a+b+carry;
+		carry=a/10;
+		ans.push_back(a%10);
+	}
+	if(carry)
+		ans.push_back(carry);
+	return ans;
+}
+
+list<int> sum_Dir2(auto L1,auto L2) {
+	L1.reverse();
+	L2.reverse();
+	list<int> ans=sum_Dir1(L1,L2);
+	ans.reverse();
+	return ans;
+}
+
+void rotate(auto& L1,int k) {
+	list<int> temp;
+	auto it=L1.begin();
+	advance(it,k);
+	temp.splice(temp.begin(),L1,it,L1.end());
+	L1.insert(L1.begin(),temp.begin(),temp.end());
 }
 
 int main() {
-	list<int> List1;
-	for(int i=0;i<10;i++) {
+	list<int> List1={7,5,9,4,6},List2={8,4};
+	/*for(int i=0;i<10;i++) {
 		List1.push_back(i+1);
 	}
 	List1.push_front(69);
@@ -75,16 +118,19 @@ int main() {
 	print_List(List1);
 	List1.reverse();
 	print_List(List1);
-	list<int> List2={5,9,6,1};
 	List1.merge(List2);
 	print_List(List1);
 	List2.clear();
 	reverse_Blocks(List1,List2,3);
 	print_List(List2);
-	it2=List2.end();
-	advance(it2,-1);
-	cout<<*it2<<endl;
-	advance(it2,1);
-	cout<<*it2<<endl;
+	cout<<"Dir1 sum:"<<"\t";
+	print_List(sum_Dir1(List1,List2));
+	List1={1,0,0};
+	List2={9,0};
+	cout<<"Dir2 sum:"<<"\t";
+	print_List(sum_Dir2(List1,List2));*/
+	
+	rotate(List1,3);
+	print_List(List1);
 	return 0;
 }
