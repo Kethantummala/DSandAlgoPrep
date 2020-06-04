0300d8c43b3cf78753d266ae66d03f504bf1d542
Kethantummala <tummala.kethan@gmail.com>
2020-06-04 20:01:45 +0530

    Started Hashing

---
+++ C++/DS/Hash.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+void findPair(int* A) {
+	int n=sizeof(A)/sizeof(A[0]);
+	unordered_multiset<int> h;
+	for(int i=0;i<n;i++) {
+		
+	}
+}
+
+int main(int argc,char* argv[]) {
+	int A[]={0, -1, 2, -3, 1};
+	findPair(A);
+	return 0;
+}
